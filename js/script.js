28514291d53ed4f40eefaf8018611f7920db4c57
IT-Ed <m.umer_ccie@outlook.com>
2016-09-02 16:08:07 +0500

    new files and folders

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("#services1").mouseenter(function(){
+		$(this).attr("src", "/images/web-design.gif");
+	});
+	$("#services1").mouseleave(function(){
+		$(this).attr("src", "/images/webdesign.png");
+	});
+});
+
+$(document).ready(function(){
+	$("#services2").mouseenter(function(){
+		$(this).attr("src", "/images/web-develop.gif");
+	});
+	$("#services2").mouseleave(function(){
+		$(this).attr("src", "/images/webdevelopment.png");
+	});
+});
+
+$(document).ready(function(){
+	$("#services3").mouseenter(function(){
+		$(this).attr("src", "/images/seo.gif");
+	});
+	$("#services3").mouseleave(function(){
+		$(this).attr("src", "/images/seo.png");
+	});
+});
