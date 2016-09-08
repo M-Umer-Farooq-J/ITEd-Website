c9f406a2096ad70c71f1b04b9706a40b650c060a
IT-Ed <m.umer_ccie@outlook.com>
2016-09-08 11:21:36 +0500

    script and other changes

--- js/script.js
+++ js/script.js
@@ -1,37 +1,29 @@
 $(document).ready(function(){
-	$("#services1").mouseenter(function(){
-		$(this).attr("src", "/images/web-design.gif");
+	$("#service-link1").mouseenter(function(){
+		$("#services1").attr("src", "/images/web-design.gif");
 	});
-	$("#services1").mouseleave(function(){
-		$(this).attr("src", "/images/webdesign.png");
+	$("#service-link1").mouseleave(function(){
+		$("#services1").attr("src", "/images/webdesign.png");
 	});
-});
 
-$(document).ready(function(){
-	$("#services2").mouseenter(function(){
-		$(this).attr("src", "/images/web-develop.gif");
+	$("#service-link2").mouseenter(function(){
+		$("#services2").attr("src", "/images/web-develop.gif");
 	});
-	$("#services2").mouseleave(function(){
-		$(this).attr("src", "/images/webdevelopment.png");
+	$("#service-link2").mouseleave(function(){
+		$("#services2").attr("src", "/images/webdevelopment.png");
 	});
-});
-
-$(document).ready(function(){
-	$("#services3").mouseenter(function(){
-		$(this).attr("src", "/images/seo.gif");
+	
+	$("#service-link3").mouseenter(function(){
+		$("#services3").attr("src", "/images/seo.gif");
 	});
-	$("#services3").mouseleave(function(){
-		$(this).attr("src", "/images/seo.png");
+	$("#service-link3").mouseleave(function(){
+		$("#services3").attr("src", "/images/seo.png");
 	});
-});
-
-$(document).ready(function(){
+	
   $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
       $(".navbar").removeClass("navbar").addClass("navbar-default navbar-fixed-top");
       //$(".navbar").switchClass("navbar", "navbar-default navbar-fixed-top", 1000);
-			
-			
     } else {
       $(".navbar-default").removeClass("navbar-fixed-top navbar-default").addClass("navbar");
     }
